5d5c0233f99c3f7fd284e504c6067cacb071d25b
NAME <mail@gmail.com>
2022-01-20 02:23:28 +0900

    commit

--- owscan.php
+++ owscan.php
@@ -33,46 +33,60 @@ $yellow    \/_____/  \,/__//__/    \/_____/  \/____/  \/__/\/_/  \/_/\/_/ $norma
 echo "\n     $yellow:::$white OWScan v1.0 - 19 March 2018 (05:50)      $normal";
 echo "\n     $yellow:::$white Author: DedSecTL/DTL | Codename: Alone.  $normal";
 echo "\n     $yellow:::$white Team: BlackHole Security | O~WEB Scanner $normal";
-echo $yellow."\n     [#]$white Enter Website to Scan$red(".$white."ex: www.example.com$red):$normal ";
+echo $yellow."\n     [#]$white Enter Website to Scan$red(".$white."ex: example.com$red):$normal ";
 $webURL = trim(fgets(STDIN));
 if ( $webURL != NULL ) {
+	// Whois Lookup
 	echo "\n\n$yellow:::$white Whois Lookup$yellow:$normal\n";
 	$whoisURL = "http://api.hackertarget.com/whois/?q=".$webURL;
 	$whoisDATA = file_get_contents($whoisURL);
 	echo $whoisDATA;
+	// DNS Lookup
 	echo "\n\n$yellow:::$white DNS Lookup$yellow:$normal\n";
 	$dnsURL = "http://api.hackertarget.com/dnslookup/?q=".$webURL;
 	$dnsDATA = file_get_contents($dnsURL);
 	echo $dnsDATA;
+	// Zone Transfer
 	echo "\n\n$yellow:::$white Zone Transfer$yellow:$normal\n";
 	$zonetransURL = "http://api.hackertarget.com/zonetransfer/?q=".$webURL;
 	$zonetransDATA = file_get_contents($zonetransURL);
 	echo $zonetransDATA;
+	// Traceroute
 	echo "\n\n$yellow:::$white Traceroute$yellow:$normal\n";
 	$traceURL = "https://api.hackertarget.com/mtr/?q=".$webURL;
 	$traceDATA = file_get_contents($traceURL);
 	echo $traceDATA;
+	// Port Scan
 	echo "\n\n$yellow:::$white PORT Scan$yellow:$normal\n";
 	$pscanURL = "http://api.hackertarget.com/nmap/?q=".$webURL;
 	$pscanDATA = file_get_contents($pscanURL);
 	echo $pscanDATA;
+	// Link Grabber
 	echo "\n\n$yellow:::$white Link Grabber$yellow:$normal\n";
 	$linkgrabURL = "https://api.hackertarget.com/pagelinks/?q=http://".$webURL;
 	$linkgrabDATA = file_get_contents($linkgrabURL);
 	echo $linkgrabDATA;
+	// IPGeolocation
 	echo "\n\n$yellow:::$white IPGeolocation$yellow:$normal\n";
 	$geoipDATA = requests("https://tools.keycdn.com/geo.json?host=".$webURL, "keycdn-tools:https://".$webURL);
 	echo $geoipDATA;
+	// HTTP Header Grabber
 	echo "\n\n$yellow:::$white HTTP Header Grabber$yellow:$normal\n";
 	$httphgrabURL = "http://api.hackertarget.com/httpheaders/?q=".$webURL;
 	$httphgrabDATA = file_get_contents($httphgrabURL);
 	echo $httphgrabDATA;
+	// Certificate Search
 	echo "\n\n$yellow:::$white Certificate Search$yellow:$normal\n";
 	$crtshData = requests("https://crt.sh/?q=".$webURL."&output=json", $ua);
 	echo $crtshData;
+	// Reverse IP Domain Check
 	echo "\n\n$yellow:::$white Reverse IP Domain Check$yellow:$normal\n";
 	$ygsrevIPData = requests("https://domains.yougetsignal.com/domains.php", $ua, 1, "remoteAddress=".$webURL."&key=");
 	echo $ygsrevIPData;
+	// BrowserSpy Report
+	echo "\n\n$yellow:::$white BrowserSpy$yellow:$normal\n";
+	$browserspyData = requests("http://browserspy.dk/webserver.php", $ua, 1, "server=".$webURL);
+	echo $browserspyData;
 	echo "\n$yellow:::$white Thanks for Using our Program\n$yellow:::$white Have a Nice Day, Bye Bye$normal\n";
 }
 else {
